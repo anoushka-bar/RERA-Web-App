a5d4dd3a25f260d00c2fbaf4cf13eb20d89c3e95
anoushka <anoushkabarthakur.2001@gmail.com>
2026-04-23 11:44:10 +0530

    Changes

--- viewer.js
+++ viewer.js
@@ -4,6 +4,10 @@ const file = params.get("file");
 
 document.getElementById("pdf-frame").src = "docs/" + file;
 
+window.onload = function() {
+  document.getElementById("viewer-container").classList.add("open");
+};
+
 // Back button
 function goBack() {
   window.location.href = "index.html";
@@ -15,3 +19,5 @@ function toggleSummary() {
   const container = document.getElementById("viewer-container");
   container.classList.toggle("open");
 }
+
+// toggleSummary();
